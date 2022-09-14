29cd0e3a5c241043ef9c62d7a7e1c3f40bca53b5
Piotr Piasecki <piterpiasecki@gmail.com>
2022-09-14 17:37:25 +0200

    deleted -it

--- init.sh
+++ init.sh
@@ -4,11 +4,11 @@
 if  ! [[ -z "$1" ]]; then
 # echo "starting $1"
 docker build --build-arg PORT=$1 -t cow .
-docker run -p 4001:$1 --name house_cow cow 
+docker run -d -p 4001:$1 --name house_cow cow 
 # echo "Cowsey is set on port $1"
 else
 docker build --build-arg PORT=8080 -t cow .
-docker run -p 4001:8080 --name house_cow cow 
+docker run -d -p 4001:8080 --name house_cow cow 
 # echo "Cowsey is set on port 8080"
 fi
 
